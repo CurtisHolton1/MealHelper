f2635ada2c7805dda3f3bcae99aee1a7e5279745
curtisholton1 <curtisholton1@outlook.com>
2015-10-16 22:35:36 -0400

    added meal name label

--- app/src/main/java/group8/mealhelper/AddMealFragment.java
+++ app/src/main/java/group8/mealhelper/AddMealFragment.java
@@ -2,9 +2,13 @@ package group8.mealhelper;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by curtis on 10/16/15.
@@ -18,8 +22,18 @@ public class AddMealFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add_meal, container, false);
+        EditText mealName = (EditText) v.findViewById(R.id.meal_name_editText);
+        mealName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                   //validate text
+                }
+            }
+        });
         return v;
     }
+
 
     @Override
     public void onClick(View v){
