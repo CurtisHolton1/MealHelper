165b5890a1d64a9feede5299f78242863d1596e5
curtisholton1 <curtisholton1@outlook.com>
2015-10-19 00:53:06 -0400

    saves picture to sd card but the pic is of low quality for some reason

--- app/src/main/java/group8/mealhelper/AddMealFragment.java
+++ app/src/main/java/group8/mealhelper/AddMealFragment.java
@@ -3,23 +3,41 @@ package group8.mealhelper;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by curtis on 10/16/15.
  */
 public class AddMealFragment extends Fragment implements View.OnClickListener {
+    private final String TAG = ((Object) this)
+            .getClass()
+            .getSimpleName().toUpperCase();
     public static int IMAGE_CAPTURED = 1;
+    public final static String APP_PATH_SD_CARD = "/MealHelper";
     ImageView imageView = null;
-    Bitmap imageBitmap = null;
+    Bitmap mImageBitmap = null;
+    String mMealName = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,13 +47,22 @@ public class AddMealFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add_meal, container, false);
-        EditText mealName = (EditText) v.findViewById(R.id.addMeal_mealName_EditText);
-        mealName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        final EditText mealNameField = (EditText) v.findViewById(R.id.addMeal_mealName_EditText);
+        mealNameField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_DONE){
+                    //Clear focus here from edittext
+                    mealNameField.clearFocus();
+                }
+                return false;
+            }
+        });
+        mealNameField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    //validate text
-                    //ie: make sure name isnt already used
+                    mMealName = mealNameField.getText().toString();
                 }
             }
         });
@@ -60,8 +87,30 @@ public class AddMealFragment extends Fragment implements View.OnClickListener {
     public void onActivityResult(int requestCode, int resultCode, Intent cameraIntent) {
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CAPTURED) {
             Bundle extras = cameraIntent.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            mImageBitmap = (Bitmap) extras.get("data");
+            String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + APP_PATH_SD_CARD;
+
+            try {
+                File dir = new File(fullPath);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                String currentDateandTime = sdf.format(new Date()).replace(" ","");
+                OutputStream fOut = null;
+                File file = new File(fullPath, currentDateandTime + ".png");
+                file.createNewFile();
+                fOut = new FileOutputStream(file);
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                // 100 means no compression, the lower you go, the stronger the compression
+                mImageBitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+                fOut.write(bytes.toByteArray());
+                fOut.flush();
+                fOut.close();
+            }catch (Exception e){
+                Log.d(TAG, "Exception in AddMealFragment: onActivityResult");
+            }
+            imageView.setImageBitmap(mImageBitmap);
             // imageBitmap=null;
         }
     }
