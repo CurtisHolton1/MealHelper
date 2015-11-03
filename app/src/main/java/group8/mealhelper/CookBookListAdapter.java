7ba897e607940682715f57c3be114548d70b5c77
curtisholton1 <curtisholton1@outlook.com>
2015-11-03 12:44:33 -0500

    working meal list, uses less memory, utilizes asyncTask

--- app/src/main/java/group8/mealhelper/CookBookListAdapter.java
+++ app/src/main/java/group8/mealhelper/CookBookListAdapter.java
@@ -3,6 +3,7 @@ package group8.mealhelper;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,27 +19,55 @@ import group8.mealhelper.models.Meal;
  * Created by curtis on 10/30/15.
  */
     public class CookBookListAdapter extends ArrayAdapter<Meal> {
-        private final Context mContext;
-        private final List<Meal> mMeals;
+    private final Context mContext;
+    private final List<Meal> mMeals;
 
-        public CookBookListAdapter(Context context, List<Meal> meals) {
-            super(context, -1, meals);
-            this.mContext = context;
-            this.mMeals = meals;
+
+    public CookBookListAdapter(Context context, List<Meal> meals) {
+        super(context, -1, meals);
+        this.mContext = context;
+        this.mMeals = meals;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.cookbook_list_row, parent, false);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.cookBook_list_row_image);
+        //BitmapFactory.Options options = new BitmapFactory.Options();
+        //options.inSampleSize = 30;
+        //Bitmap bitmap = BitmapFactory.decodeFile(mMeals.get(position).getPathToPic(), options);
+        String[] path = {mMeals.get(position).getPathToPic()};
+        new LocalPicTask(imageView).execute(path);
+        TextView textView = (TextView) rowView.findViewById(R.id.cookBook_list_row_textView);
+        textView.setText(mMeals.get(position).getName());
+        return rowView;
+    }
+
+    private class LocalPicTask extends AsyncTask<String,Void,Bitmap> {
+        ImageView mImageView;
+        public LocalPicTask(ImageView imageView){
+            this.mImageView = imageView;
+        }
+        @Override
+        protected void onPreExecute(){
+
         }
 
         @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.cookbook_list_row, parent, false);
-            ImageView imageView = (ImageView) rowView.findViewById(R.id.cookBook_list_row_image);
+        protected Bitmap doInBackground(String... params){
             BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 2;
-            Bitmap bitmap = BitmapFactory.decodeFile(mMeals.get(position).getPathToPic(), options);
-            imageView.setImageBitmap(bitmap);
-            TextView textView = (TextView) rowView.findViewById(R.id.cookBook_list_row_textView);
-            textView.setText(mMeals.get(position).getName());
-            return  rowView;
+            options.inSampleSize = 30;
+            Bitmap bitmap = BitmapFactory.decodeFile(params[0], options);
+            return  bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result){
+            super.onPostExecute(result);
+            mImageView.setImageBitmap(result);
         }
     }
+}
+
 
