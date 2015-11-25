28f17e363df80e5746d93ae1ddd8d42f24b14550
curtisholton1 <curtisholton1@outlook.com>
2015-11-24 22:20:57 -0500

    Meal pics in async task for day view

--- app/src/main/java/group8/mealhelper/DayFragment.java
+++ app/src/main/java/group8/mealhelper/DayFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -120,35 +121,53 @@ public class DayFragment extends Fragment implements View.OnClickListener{
     private void setBreakfastView(Meal breakfast){
         TextView textView = (TextView)mBreakfastMealView.findViewById(R.id.single_meal_textView);
         textView.setText(breakfast.getName());
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 32;
-        Bitmap bitmap = BitmapFactory.decodeFile(breakfast.getPathToPic(), options);
         ImageView imageView = (ImageView) mBreakfastMealView.findViewById(R.id.single_meal_image);
-        imageView.setImageBitmap(bitmap);
+        String[] path = {breakfast.getPathToPic()};
+        new SetPicTask(imageView).execute(path);
         mBreakfastMealView.setVisibility(View.VISIBLE);
     }
 
     private void setLunchView(Meal lunch){
         TextView textView = (TextView)mLunchMealView.findViewById(R.id.single_meal_textView);
         textView.setText(lunch.getName());
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 32;
-        Bitmap bitmap = BitmapFactory.decodeFile(lunch.getPathToPic(), options);
         ImageView imageView = (ImageView) mLunchMealView.findViewById(R.id.single_meal_image);
-        imageView.setImageBitmap(bitmap);
+        String[] path = {lunch.getPathToPic()};
+        new SetPicTask(imageView).execute(path);
         mLunchMealView.setVisibility(View.VISIBLE);
     }
 
     private void setDinnerView(Meal dinner){
         TextView textView = (TextView)mDinnerMealView.findViewById(R.id.single_meal_textView);
         textView.setText(dinner.getName());
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 32;
-        Bitmap bitmap = BitmapFactory.decodeFile(dinner.getPathToPic(), options);
         ImageView imageView = (ImageView) mDinnerMealView.findViewById(R.id.single_meal_image);
-        imageView.setImageBitmap(bitmap);
+        String[] path = {dinner.getPathToPic()};
+        new SetPicTask(imageView).execute(path);
         mDinnerMealView.setVisibility(View.VISIBLE);
     }
 
+    private class SetPicTask extends AsyncTask<String,Void,Bitmap> {
+        ImageView mImageView;
+        public SetPicTask(ImageView imageView){
+            this.mImageView = imageView;
+        }
+        @Override
+        protected void onPreExecute(){
+
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params){
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 32;
+            Bitmap bitmap = BitmapFactory.decodeFile(params[0], options);
+            return  bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result){
+            super.onPostExecute(result);
+            mImageView.setImageBitmap(result);
+        }
+    }
 
 }
